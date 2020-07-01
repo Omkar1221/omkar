15c556be858022adab46d1fa69020f4c4e988c20
Omkar1221 <67649925+Omkar1221@users.noreply.github.com>
2020-07-01 09:35:15 +0530

    Update login.php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title>Omkar Rajesh Shinde c3d13b69</title>
+<title>Omkar Rajesh Shinde</title>
 </head>
 <body>
 <div class="container">
